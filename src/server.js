080e2fee30d964d27885fb01633b1f5185acd09f
DavidSauner <davidsauner@gmail.com>
2023-01-18 16:31:45 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,31 @@
+const PORT = 5000;
+import express from "express";
+import { MongoClient, ObjectId } from "mongodb";
+import dotenv from "dotenv";
+import cors from "cors";
+import joi from "joi";
+
+const app = express();
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+
+try {
+    await mongoClient.connect();
+    } catch (err) {
+    console.log("Erro no mongo.conect", err.message);
+    }
+    db = mongoClient.db();
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`Server running in port: ${PORT}`));
